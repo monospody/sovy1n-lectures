f838831ce8ffe82673017aa49adf5dddd26d794a
monospody <monika.podhajecka@akademiasovy.sk>
2020-01-02 17:27:36 +0100

    exercise 1,2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import "./styles.css";
 
 console.log("hello");
+let count = 1;
+function plus(){
+
+	fetch('https://swapi.co/api/people/'+(++count))
+	  .then((response) => {
+		return response.json();
+	  })
+	  .then((myJson) => {
+		console.log(myJson);
+		document.getElementById("output").innerHTML = myJson.name;
+	  });
+}
+function minus(){
+
+	fetch('https://swapi.co/api/people/'+(--count))
+	  .then((response) => {
+		return response.json();
+	  })
+	  .then((myJson) => {
+		console.log(myJson);
+		document.getElementById("output").innerHTML = myJson.name;
+	  });
+}
+
+document.getElementById("plus").addEventListener("click",plus,false);
+document.getElementById("minus").addEventListener("click",minus,false);
